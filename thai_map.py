49f7dba7930070bcbe5847551873fea0c31ef7dd
nnbaitei <nnbaitei@gmail.com>
2024-07-06 03:13:04 +0700

    remove map background

--- thai_map.py
+++ thai_map.py
@@ -2,44 +2,42 @@ import json
 import urllib.request
 import numpy as np
 import plotly.graph_objects as go
-import dash_leaflet as dl
-import dash_leaflet.express as dlx
-from dash import Dash, html, dcc, Output, Input
-from dash_extensions.javascript import assign
+from dash import Dash, html, dcc
 
-
-#Load and read the geojson file for Thailand regions. 
+# Load and read the geojson file for Thailand regions.
 italy_url = "https://raw.githubusercontent.com/chingchai/OpenGISData-Thailand/master/provinces.geojson"
 with urllib.request.urlopen(italy_url) as url:
-        jdata = json.loads(url.read().decode())
-              
-pts = []#list of points defining boundaries of polygons
-for  feature in jdata['features']:
-    if feature['geometry']['type'] == 'Polygon':
-        pts.extend(feature['geometry']['coordinates'][0])    
-        pts.append([None, None])#mark the end of a polygon   
-        
-    elif feature['geometry']['type'] == 'MultiPolygon':
-        for polyg in feature['geometry']['coordinates']:
-            pts.extend(polyg[0])
-            pts.append([None, None])#end of polygon
-    elif feature['geometry']['type'] == 'LineString': 
-        pts.extend(feature['geometry']['coordinates'])
-        pts.append([None, None])
-    else: pass           
-    #else: raise ValueError("geometry type irrelevant for map")
-
-x, y = zip(*pts)    
+    jdata = json.loads(url.read().decode())
 
 fig = go.Figure()
-fig.add_scatter(x=x, y=y, mode='lines', line_color='#999999', line_width=1.5)
-fig.update_layout(width=600,  height=800)
+
+# Loop through each feature in the geojson file
+for feature in jdata['features']:
+    if feature['geometry']['type'] == 'MultiPolygon':
+        for polyg in feature['geometry']['coordinates']:
+            pts = polyg[0]
+            x, y = zip(*pts)
+            pro_name = feature['properties']['pro_en']
+            if pro_name == "Songkhla":  # Check if the province is Songkhla
+                fig.add_scatter(x=x, y=y, mode='lines', line_color='red', line_width=1.5, fill='toself', fillcolor='rgba(255, 0, 0, 0.2)', text=pro_name)
+            else:
+                fig.add_scatter(x=x, y=y, mode='lines', line_color='#999999', line_width=1.5, fill='toself', fillcolor='rgba(200, 200, 200, 0.2)', text=pro_name)
+
+fig.update_layout(width=600, 
+                  height=900, 
+                  xaxis_showgrid=False, 
+                  yaxis_showgrid=False, 
+                  showlegend=False, 
+                  template='plotly_dark',
+                  plot_bgcolor='rgba(0, 0, 0, 0)',
+                  paper_bgcolor='rgba(0, 0, 0, 0)')
+fig.update_xaxes(showticklabels=False)
+fig.update_yaxes(showticklabels=False)
 
 app = Dash()
 app.layout = html.Div([
     dcc.Graph(figure=fig)
 ])
-
 
 if __name__ == '__main__':
     app.run_server()
